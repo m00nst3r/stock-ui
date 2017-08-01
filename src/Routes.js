7364b30943635cfd886028f0b9a2a9601fe6dd36
Andrew <akolisnyk@malkosua.com>
2017-08-01 23:24:12 +0200

    start working on admin page and change hashHistory to browserHistory

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 import App from './app/App'
 import Login from './app/Login'
+import Admin from './app/admin/AdminPage'
 
 const routers = (
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
         <Route path="/" component={App}/>
         <Route path="/login" component={Login}/>
+        <Route path="/a" component={Admin}/>
     </Router>
 );
 
---
+++ src/app/admin/AdminPage.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class AdminPage extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default AdminPage;
